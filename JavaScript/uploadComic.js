fe43cf157fb0ca21bb95a4c1f6e7114048ada726
ericalundstrom <erica.lundstrom99@hotmail.com>
2023-11-28 09:59:43 +0100

    upd

--- JavaScript/uploadComic.js
+++ JavaScript/uploadComic.js
@@ -1,82 +1,84 @@
-async function renderUploadComic(){
+async function renderUploadComic() {
 
-        swapStyleSheet("css/uploadComic.css");
+    swapStyleSheet("css/uploadComic.css");
 
     document.querySelector("#wrapper").innerHTML = `
         <input name="title "placeholder="title">
         <input name="description" placeholder="description">
         <div id="filters"> </div>
-        <form id="upload" action="upload.php" method="POST" enctype="multipart/form-data">
+        <form id="upload" action="api/upload.php" method="POST" enctype="multipart/form-data">
                 <input type="file" name="comic">
                 <button type="submit">Upload</button>
-            </form>
+        </form>
         <div id="frontPage"></div>
     `
-
     const response = await fetch("api/data/filters.json");
     let filterData = await response.json();
     const container = document.querySelector("#filters");
 
-
-for (const key in filterData) {
-    const dropDownDOM = document.createElement("div");
-    contentButtonDom = document.createElement("button");
-    contentButtonDom.textContent = key;
-    contentButtonDom.addEventListener("click", () => {
-        renderOptions(filterData[key], dropDownDOM);
-    })
-    dropDownDOM.classList.add("dropDown");
-    container.append(dropDownDOM);
-    dropDownDOM.append(contentButtonDom);
-
-
-function renderOptions(key, container){
-    key.forEach( value => {
-        const tagDOM = document.createElement("href");
-        tagDOM.textContent = value;
-        container.append(tagDOM);
-    })
-    if(Array.isArray(key)){
-        key.forEach(value => {
-        const underTagDOM = document.createElement("href");
-        underTagDOM.textContent = value;
-        tagDOM.append(underTagDOM);
-            })
-        }
-    }
- }
-
-let user = localStorager.get_item("user");
-
-const title = document.querySelector("input[name='title']");
-const  description = document.querySelector("input[name='description']");
-const frontPage = document.querySelector("#frontPage")
-const form = document.querySelector("form");
-
-form.addEventListener("submit", async (event) => {
-    event.preventDefault();
-
-    const formData = new FormData(form);
-
-    try {
-        const response = await fetch("api/upload.php", {
-            method: "POST",
-            body: formData
+    for (const key in filterData) {
+        const dropDownDOM = document.createElement("div");
+        contentButtonDom = document.createElement("button");
+        contentButtonDom.textContent = key;
+        contentButtonDom.addEventListener("click", () => {
+            renderOptions(filterData[key], dropDownDOM);
         });
+        dropDownDOM.classList.add("dropDown");
+        container.append(dropDownDOM);
+        dropDownDOM.append(contentButtonDom);
 
-        const data = await response.json();
-
-        if (data.error) {
-            console.error(data.error);
-        } else {
-            const img = document.createElement("img");
-            img.src = data.image; 
-            frontPage.append(img);
+        function renderOptions(key, container) {
+            key.forEach(value => {
+                const tagDOM = document.createElement("a");
+                tagDOM.textContent = value;
+                container.append(tagDOM);
+            });
+            if (Array.isArray(key)) {
+                key.forEach(value => {
+                    const underTagDOM = document.createElement("a");
+                    underTagDOM.textContent = value;
+                    tagDOM.append(underTagDOM);
+                });
+            }
         }
-    } catch (error) {
-        console.error("Error uploading image:", error);
     }
-});
+
+    let user = localStorage.getItem("user");
+
+    const title = document.querySelector("input[name='title']");
+    const description = document.querySelector("input[name='description']");
+    const frontPage = document.querySelector("#frontPage")
+    const form = document.querySelector("form");
+
+    form.addEventListener("submit", async (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(form);
+        console.log(formData);
+
+        try {
+            const request = new Request("api/upload.php", {
+                method: "POST",
+                body: formData
+            });
+
+            let response = await fetch(request);
+
+            const data = await response.json();
+            console.log(data);
+            // console.log(data.comic);
+
+            if (data.error) {
+                console.error(data.error);
+            } else {
+                const img = document.createElement("img");
+                img.src = `../api/${data}`;
+                frontPage.append(img);
+            }
+        } catch (error) {
+            console.error("Error uploading image:", error);
+        }
+    });
 
 
 
--- api/upload.php
+++ api/upload.php
@@ -4,45 +4,57 @@ ini_set("display_errors", 1);
 
 require_once("functions.php");
 
-if ($_SERVER["REQUEST_METHOD"] === "POST"){
-    if(isset($_FILES["file"])) {
+// function send_JSON ($data, $code = 200){
+//   header("Content-Type: application/json");
+//   http_response_code($code);
+//   echo json_encode($data);
+//   exit();
+// }
 
+if ($_SERVER["REQUEST_METHOD"] === "POST"){
+
+  if(isset($_FILES["comic"])) {
+    
 
     $file = $_FILES["comic"];
 
-  $fileName = $file["name"];
-  $fileTmpName = $file["tmp_name"];
-  $fileSize = $file["size"];
-  $fileError = $file["error"];
-  $fileType = $file["type"];
+    $fileName = $file["name"];
+    $fileTmpName = $file["tmp_name"];
+    $fileSize = $file["size"];
+    $fileError = $file["error"];
+    $fileType = $file["type"];
 
-  $fileExtension = pathinfo($fileName, PATHINFO_EXTENSION);
+    $fileExtension = pathinfo($fileName, PATHINFO_EXTENSION);
 
-  $allowed = ["jpg", "jpeg", "png"];
-  if(in_array($fileExtension, $allowed)) {
+    $allowed = ["jpg", "jpeg", "png", "image/jpg","JPG"];
 
-    if($fileSize < 2000000) {
-      if($fileError === 0) {
-        $destination = "data/comics/".$fileName;
-        $source = $fileTmpName;
+    if(in_array($fileExtension, $allowed)) {
+      if($fileSize < 2000000) {
+        if($fileError === 0) {
+       
+          $destination = "data/comics/".$fileName;
+          $source = $fileTmpName;
 
-        if(move_uploaded_file($source, $destination)) {
-          $imageSource = "data/comics/".$fileName;         
-          sendJSON($imageSource, 200);
+          if(move_uploaded_file($source, $destination)) {
+            $imageSource = "data/comics/".$fileName;   
+            $erica = ["erica" => "flyttad"];
+            send_JSON($destination);      
+            send_JSON($imageSource);
+          }
+        } else {
+          send_JSON($fileError);
+          $error = ["error" => "Something went wrong when uploading image."];
+          send_JSON($error, 400);
         }
       } else {
-        $error = ["error" => "Something went wrong when uploading image."];
-        sendJSON($error, 400);
+        $error = ["error" => "The file you uploaded is to big."];
+        send_JSON($error, 413);
       }
-    } else {
-      $error = ["error" => "The file you uploaded is to big."];
-      sendJSON($error, 413);
-    }
-  } else {
+      } else {
         $error = ["error" => "We only allow JPG, JPEG & PNG files."];
-        sendJSON($error, 405);
+        send_JSON($error, 405);
+       }
     }
-  }
 }
 
 
